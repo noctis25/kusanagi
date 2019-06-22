bfc85b04cc16836bfa48ba894d709ca3e4c1a354
noctis25 <ikechuku16@gmail.com>
2019-06-22 13:45:54 -0400

    adding gitignore removing dependencies

--- server/app.js
+++ server/app.js
@@ -1,41 +1,29 @@
+//Setup express server
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+
+//Grab Jikan API
 const Jikan = require('jikan-node');
 const mal = new Jikan();
-const AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'us-east-1'});
-const unmarshalItem = require('dynamodb-marshaler').unmarshalItem;
 
-// Create the DynamoDB service object
+//Setup AWS
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
 const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-const params = {
-  TableName: 'anime',
-  Key: {
-    'id': {N: '1'}
-  }
-};
+//Unmarshaller for dynamo
+const unmarshalItem = require('dynamodb-marshaler').unmarshalItem;
 
-let akira = [];
+//Items to hold dynamo items
+let items = [];
 
-// Call DynamoDB to read the item from the table
-ddb.getItem(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    akira.push(data.Item);
-   // console.log("Success", data.Item);
-  }
-});
-
+//db scan
 let data = ddb.scan({
   TableName: 'anime'
 }, function(err, data) {
-  // data.Items = [{username: {S: 'nackjicholson'}]
-  let items = data.Items.map(unmarshalItem);
-  console.log(items); // [{username: 'nackjicholson'}]
+  items = data.Items.map(unmarshalItem);
+  console.log(items); 
 });
 
 // console.log that your server is up and running
@@ -43,13 +31,12 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 let baga = '';
 
 
+//Sample jikan api search
 mal.search('anime','fullmetal alchemist', {limit: 5})
     .then(search => baga = search)
     .catch(err => console.log(err));
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
-  res.send({express: akira});
+  res.send({express: items});
 });
-
-//todo - load your own shit and then send to aws db
