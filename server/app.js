bd07eba4247864f73fe625110a24ec764b776420
= <kevinbryant@Kevins-MacBook-Pro.local>
2019-07-08 21:11:05 -0400

    Added functionality for user to enter new data and then persist to db

--- server/app.js
+++ server/app.js
@@ -7,6 +7,21 @@ const port = process.env.PORT || 4000;
 const Jikan = require('jikan-node');
 const mal = new Jikan();
 
+// Create body parser
+const bodyParser = require('body-parser')
+
+//Create multer for multi-part/form-data
+const multer = require('multer');
+var upload = multer(); 
+
+// to support JSON-encoded bodies 
+app.use( bodyParser.json() ); 
+app.use(bodyParser.urlencoded({})); 
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+app.use(express.static('public'));
+
 //Setup AWS
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
@@ -37,4 +52,28 @@ mal.search('anime','fullmetal alchemist', {limit: 1})
 // create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({express: items});
+});
+
+app.post("/inputData", (req, res) => {
+  
+  var params = {
+    TableName: 'anime',
+    Item: {
+      'id' : {N: '3'},
+      "url": {S: req.body.url},
+     "image_url":{S: req.body.image_url},
+     "title":{S: req.body.title},
+     "synopsis": {S: req.body.synopsis}
+    }
+  };
+  
+   res.send("recieved your request!");
+   //Write req.body to database
+   ddb.putItem(params, function(err, data) {
+     if (err) {
+       console.log("Error", err);
+     } else {
+       console.log("Success", data);
+     }
+   });
 });
